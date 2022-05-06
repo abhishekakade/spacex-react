b3b305bb7df9e329716730f8ab32b9f3897fe4ea
abhishekakade <26276153+abhishekakade@users.noreply.github.com>
2022-05-06 11:22:17 +0530

    fix: react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { createContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Launches from "./components/Launches";
 import useFetchDetails from "./Hooks/useFetchDetails";
 
-// changed
 export const DataContext = createContext();
 
 function App() {
-  // changed
   // all launchpads data
   const launchpadsURL = "https://api.spacexdata.com/v4/launchpads";
   const allLaunchpadsData = useFetchDetails(launchpadsURL);
@@ -18,40 +16,35 @@ function App() {
   const allLaunchesData = useFetchDetails(launchesURL);
   // console.log(allLaunchesData);
 
-  // before change
-  // let params = useParams();
-  // console.log(params);
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <DataContext.Provider
-              value={{
-                allLaunches: allLaunchesData,
-                allLaunchpads: allLaunchpadsData,
-              }}
-            >
-              <Home />
-            </DataContext.Provider>
-          }
-        />
-        <Route
-          path="/launches/:launchId"
-          element={
-            <DataContext.Provider
-              value={{
-                allLaunches: allLaunchesData,
-                allLaunchpads: allLaunchpadsData,
-              }}
-            >
-              <Launches />
-            </DataContext.Provider>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <DataContext.Provider
+            value={{
+              allLaunches: allLaunchesData,
+              allLaunchpads: allLaunchpadsData,
+            }}
+          >
+            <Home />
+          </DataContext.Provider>
+        }
+      />
+      <Route
+        path="/launches/:launchId"
+        element={
+          <DataContext.Provider
+            value={{
+              allLaunches: allLaunchesData,
+              allLaunchpads: allLaunchpadsData,
+            }}
+          >
+            <Launches />
+          </DataContext.Provider>
+        }
+      />
+    </Routes>
   );
 }
 
